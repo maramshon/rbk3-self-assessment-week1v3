c562f9ff502729d371de5faa42db637c1731acd7
maramshon <mralshon@gmail.com>
2017-09-27 09:16:00 +0300

    hsfjas

--- ds-stack/ds-stack.js
+++ ds-stack/ds-stack.js
@@ -1,9 +1,3 @@
 var Stack = function() {
-    this._storage = [];
-    this.add = function(value){
-      this._storage.push(value);
-    };
-    this.remove = function() {
-      this._storage.pop();
-    };
+    this._s
   };
